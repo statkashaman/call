d89f693113a10352be031def07386e265c697076
Shaman <statkashaman@mail.ru>
2014-02-26 22:21:01 +0700

    work select

--- askq/forms.py
+++ askq/forms.py
@@ -3,5 +3,7 @@ from django import forms
 from models import Proj
 
 class ProjForm(forms.ModelForm):
-   class Meta:
-       model = Proj
+    projmod = forms.ModelChoiceField(queryset=Proj.objects.all(), empty_label="Выберите проект")
+    class Meta:
+        model = Proj
+
