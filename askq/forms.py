2c22069cb17038a5e1d8945a98be595a1cfe2466
sohan <statkashaman@mail.ru>
2014-02-27 13:29:16 +0700

    ver 1.03

--- askq/forms.py
+++ askq/forms.py
@@ -3,7 +3,9 @@ from django import forms
 from models import Proj
 
 class ProjForm(forms.ModelForm):
-    projmod = forms.ModelChoiceField(queryset=Proj.objects.all(), empty_label="Выберите проект")
     class Meta:
         model = Proj
-
+        exclude = ('url',)
+        widgets = {
+            'projectname': forms.Select( choices=Proj.objects.all()),
+        }
--- askq/views.py
+++ askq/views.py
@@ -6,15 +6,12 @@ from askq.forms import ProjForm
 class Projects(ListView):
     model = Proj
     context_object_name = 'projects'
-    template_name = 'askq\index.html'
+    template_name = 'askq/index.html'
+    def index(request):
+        return render_to_response('askq/index.html', {'form':ProjForm()}, context_instance=RequestContext(request))
+
 
 class Asks(DetailView):
     model = Ask
-    template_name = 'askq\detail.html'
+    template_name = 'askq/detail.html'
 
-def index(request):
-    form = ProjForm(request.GET)
-    if form.is_valid():
-        form.save()
-    context = dict(form=form)
-    return render_to_response('askq\index.html', context)
