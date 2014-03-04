fbd341a19cff745f9302c33f38ab4befc0deea3f
sohan <statkashaman@mail.ru>
2014-03-04 11:55:58 +0700

    ver 2.01

--- askq/views.py
+++ askq/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from askq.models import Proj, Ask
@@ -7,13 +7,17 @@ from askq.forms import ProjForm
 
 def index(request):
     if 'projectname' in request.GET:
-        prname = request.GET['projectname']
-        askq = Ask.objects.filter(proj=prname)
-        askqs = Ask.objects.all()
-        projects = Proj.objects.all()
-        cur_proj = Proj.objects.filter(id=prname)
-        form = ProjForm()
-        context = {'form': form , 'projects': projects, 'askq': askq, 'askqs': askqs, 'cur_proj': cur_proj}
+        if request.GET['projectname'] == '':
+            return HttpResponseRedirect("/call/")
+        else:
+            prname = request.GET['projectname']
+            askq = Ask.objects.filter(proj=prname)
+            askqs = Ask.objects.all()
+            projects = Proj.objects.all()
+            cur_proj = Proj.objects.get(id=prname)
+            form = ProjForm()
+            form.fields['projectname'].initial = prname
+            context = {'form': form , 'projects': projects, 'askq': askq, 'askqs': askqs, 'cur_proj': cur_proj}
         return render(request, 'askq/index.html', context)
     else:
         projects = Proj.objects.all()
