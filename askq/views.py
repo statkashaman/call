46d369682ef613b87eb52bf812ef1e56957dd909
sohan <statkashaman@mail.ru>
2014-03-06 22:04:40 +0700

    ver 2.04 search

--- askq/views.py
+++ askq/views.py
@@ -1,6 +1,6 @@
+# coding=utf-8
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
 from askq.models import Proj, Ask, Answer
 from askq.forms import ProjForm
 
@@ -9,12 +9,8 @@ def index(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
         sq = Ask.objects.filter(question__icontains=q)
-        if sq == '':
-            search_status = '0'
-        else:
-            search_status = '1'
         form = ProjForm()
-        context = {'form': form,'sq': sq, 'query': q, 'search_status': search_status}
+        context = {'form': form,'sq': sq, 'query': q}
         return render(request,'askq/index.html',context)
     else:
         if 'projectname' in request.GET:
@@ -27,23 +23,20 @@ def index(request):
                     askq = Ask.objects.filter(proj=prname)
                     cur_ask = Ask.objects.get(id=cur_quest)
                     answers = Answer.objects.get(ask_id=cur_quest)
-                    projects = Proj.objects.all()
                     cur_proj = Proj.objects.get(id=prname)
                     form = ProjForm()
                     form.fields['projectname'].initial = prname
-                    context = {'form': form , 'projects': projects, 'askq': askq, 'cur_proj': cur_proj, 'cur_quest': cur_quest, 'answer': answers,'cur_ask': cur_ask}
+                    context = {'form': form , 'askq': askq, 'cur_proj': cur_proj, 'cur_quest': cur_quest, 'answer': answers,'cur_ask': cur_ask}
                     return render(request, 'askq/index.html', context)
                 else:
                     prname = request.GET['projectname']
                     askq = Ask.objects.filter(proj=prname)
-                    projects = Proj.objects.all()
                     cur_proj = Proj.objects.get(id=prname)
                     form = ProjForm()
                     form.fields['projectname'].initial = prname
-                    context = {'form': form , 'projects': projects, 'askq': askq, 'cur_proj': cur_proj}
+                    context = {'form': form , 'askq': askq, 'cur_proj': cur_proj}
                     return render(request, 'askq/index.html', context)
         else:
-#            projects = Proj.objects.all()
             form = ProjForm()
             context = {'form': form}
             return render(request, 'askq/index.html', context)
